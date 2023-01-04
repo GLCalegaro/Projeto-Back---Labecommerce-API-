7029c4501a1552efc9f3b120c0c17e6940c9b490
GLCalegaro <giovaluiza@gmail.com>
2023-01-04 17:19:56 -0300

    Exercícios typescript II

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export enum Category {
+    ACESSORIES = "Acessories",
+    CLOTHES_AND_SHOES = "Roupas e calçados",
+    ELECTRONICS = "Eletrônicos"
+    }
+
+export type TUser = {
+    id: string,
+    email: string,
+    password: string,
+  }
+
+export type TProduct = {
+    id: string,
+    name: string,
+    price: number,
+    category: Category,
+}
+
+export type TPurchase = {
+    userId: string,
+    productId: string,
+    quantity: number,
+    totalPrice: number,
+}
